2032e3c7a44b90c338b627cad9766e6048527f98
Saul Medeiros <saulmedeiros2017@gmail.com>
2023-06-20 20:03:28 -0300

    configurando testes de persistencia da classe atendente

--- src/testes/atendente/TestaAtendenteDAOleUm.java
+++ src/testes/atendente/TestaAtendenteDAOleUm.java
@@ -1,10 +1,13 @@
 package testes.atendente;
 
+import persistencia.AtendenteDAO;
+
 public class TestaAtendenteDAOleUm {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
-        
+        // matricula
+        System.out.println(AtendenteDAO.leUm(0));
         
     }
     
