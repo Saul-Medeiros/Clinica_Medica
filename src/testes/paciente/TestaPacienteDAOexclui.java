d0a764a4ca0fc5a041aea0588bdf5754953018c7
Saul Medeiros <saulmedeiros2017@gmail.com>
2023-06-22 14:36:50 -0300

    Inserindo imagem do diagrama de classes e do modelo relacional do MySQL Workbench

--- src/testes/paciente/TestaPacienteDAOexclui.java
+++ src/testes/paciente/TestaPacienteDAOexclui.java
@@ -6,7 +6,7 @@ public class TestaPacienteDAOexclui {
 
     public static void main(String[] args) throws Exception {
         
-        // String
+        // String(cpf)
         System.out.println(PacienteDAO.exclui("55555555555"));
         
     }
